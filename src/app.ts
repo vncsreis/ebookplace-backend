1ca9836f068f9bddc6267d00c39594882e128ae5
Vinícius <euphonion@tutanota.com>
2022-02-02 00:15:44 -0300

    Urlencoded added to app

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/', router);
 
 app.listen(3030, () => console.log('Listening on port 3030'));
