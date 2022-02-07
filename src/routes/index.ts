0fae0d247dedb3acadafe4c52c680489ed6e6c55
Vinícius <euphonion@tutanota.com>
2022-02-06 23:58:58 -0300

    Added requireLogin to routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,14 @@ import { userRouter } from './userRouter';
 import { bookRouter } from './bookRouter';
 import { genreRouter } from './genreRouter';
 import { loginRouter } from './loginRouter';
+import { requireLogin } from '../middleware/requireLogin';
 
 export const router = Router();
 
-router.use('/user', userRouter);
+router.use('/user', requireLogin, userRouter);
 
-router.use('/book', bookRouter);
+router.use('/book', requireLogin, bookRouter);
 
-router.use('/genre', genreRouter);
+router.use('/genre', requireLogin, genreRouter);
 
 router.use('/login', loginRouter);
