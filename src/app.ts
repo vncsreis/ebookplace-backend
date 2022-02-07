8d1c94e0fb161a666de59af15ccbdc0ce3340820
Vinícius <euphonion@tutanota.com>
2022-02-06 23:19:07 -0300

    Separated server function to own file

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import { router } from './routes';
 
-const app = express();
+export const app = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router);
-
-app.listen(3030, () => console.log('Listening on port 3030'));
---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import { app } from './app';
+
+app.listen(3030, () => console.log('Listening on port 3030'));
