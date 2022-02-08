0895785866ad8ad05461876d138b4bb38246b792
Vinícius <euphonion@tutanota.com>
2022-02-07 23:23:35 -0300

    Genre controller and route refactor

--- src/controllers/GenreController.ts
+++ src/controllers/GenreController.ts
@@ -1,36 +1,63 @@
+import { Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { prisma } from '../../prisma/client';
 import { GenreInfoInput } from '../models/GenreInfoInput';
+import { getErrorMessage } from '../utilities/getErrorMessage';
 
 class GenreController {
-  async getGenres() {
-    const genres = await prisma.genre.findMany();
-    return genres;
+  async getGenres(req: Request, res: Response) {
+    try {
+      const genres = await prisma.genre.findMany();
+      res.status(200).json(genres);
+    } catch (e) {
+      res.status(400).json({ error: getErrorMessage(e) });
+    }
   }
 
-  async getGenreById(id: string) {
-    const genre = await prisma.genre.findUnique({ where: { id } });
-    return genre;
+  async getGenreById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const genre = await prisma.genre.findUnique({ where: { id } });
+      res.status(200).json(genre);
+    } catch (e) {
+      res.status(400).json({ error: getErrorMessage(e) });
+    }
   }
 
-  async createGenre(genre: GenreInfoInput) {
-    const createdGenre = await prisma.genre.create({
-      data: { id: uuidv4(), ...genre },
-    });
-    return createdGenre;
+  async createGenre(req: Request, res: Response) {
+    try {
+      const genre: GenreInfoInput = req.body;
+      const createdGenre = await prisma.genre.create({
+        data: { id: uuidv4(), ...genre },
+      });
+      res.status(200).json(createdGenre);
+    } catch (e) {
+      res.status(400).json({ error: getErrorMessage });
+    }
   }
 
-  async updateGenre(id: string, genre: GenreInfoInput) {
-    const updatedGenre = await prisma.genre.update({
-      where: { id },
-      data: { ...genre },
-    });
-    return updatedGenre;
+  async updateGenre(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const genre: GenreInfoInput = req.body;
+      const updatedGenre = await prisma.genre.update({
+        where: { id },
+        data: { ...genre },
+      });
+      res.status(200).json(updatedGenre);
+    } catch (e) {
+      res.status(400).json({ error: getErrorMessage });
+    }
   }
 
-  async deleteGenre(id: string) {
-    const deletedGenre = await prisma.genre.delete({ where: { id } });
-    return deletedGenre;
+  async deleteGenre(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const deletedGenre = await prisma.genre.delete({ where: { id } });
+      res.status(200).json(deletedGenre);
+    } catch (e) {
+      res.status(400).json({ error: getErrorMessage(e) });
+    }
   }
 }
 
--- src/routes/genreRouter.ts
+++ src/routes/genreRouter.ts
@@ -3,50 +3,12 @@ import { genreController } from '../controllers/GenreController';
 
 export const genreRouter = Router();
 
-genreRouter.get('/', async (req, res) => {
-  try {
-    const genres = await genreController.getGenres();
-    res.send(JSON.stringify(genres));
-  } catch (e) {
-    res.status(400).send(JSON.stringify(e));
-  }
-});
+genreRouter.get('/', genreController.getGenres);
 
-genreRouter.get('/:id', async (req, res) => {
-  try {
-    const genre = await genreController.getGenreById(req.params.id);
-    res.send(JSON.stringify(genre));
-  } catch (e) {
-    res.status(400).send(JSON.stringify(e));
-  }
-});
+genreRouter.get('/:id', genreController.getGenreById);
 
-genreRouter.post('/', async (req, res) => {
-  try {
-    const createdGenre = await genreController.createGenre(req.body);
-    res.send(JSON.stringify(createdGenre));
-  } catch (e) {
-    res.status(400).send(JSON.stringify(e));
-  }
-});
+genreRouter.post('/', genreController.createGenre);
 
-genreRouter.put('/:id', async (req, res) => {
-  try {
-    const updatedGenre = await genreController.updateGenre(
-      req.params.id,
-      req.body,
-    );
-    res.send(JSON.stringify(updatedGenre));
-  } catch (e) {
-    res.status(400).send(JSON.stringify(e));
-  }
-});
+genreRouter.put('/:id', genreController.updateGenre);
 
-genreRouter.delete('/:id', async (req, res) => {
-  try {
-    const deletedGenre = await genreController.deleteGenre(req.params.id);
-    res.send(JSON.stringify(deletedGenre));
-  } catch (e) {
-    res.status(400).send(JSON.stringify(e));
-  }
-});
+genreRouter.delete('/:id', genreController.deleteGenre);
