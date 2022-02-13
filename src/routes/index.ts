fd3f52e2ecfbca438ca380740b3f6fb932fcc4e3
Vinícius <euphonion@tutanota.com>
2022-02-13 18:11:08 -0300

    Add static file serving

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,8 @@ import { bookRouter } from './bookRouter';
 import { genreRouter } from './genreRouter';
 import { loginRouter } from './loginRouter';
 import { requireLogin } from '../middleware/requireLogin';
+import path from 'path';
+import { getErrorMessage } from '../utilities/getErrorMessage';
 
 export const router = Router();
 
@@ -14,3 +16,21 @@ router.use('/book', requireLogin, bookRouter);
 router.use('/genre', requireLogin, genreRouter);
 
 router.use('/login', loginRouter);
+
+router.get('/static/:id', requireLogin, (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, '..', '..', 'uploads', req.params.id));
+  } catch (e) {
+    res.status(400).json({ error: getErrorMessage(e) });
+  }
+});
+
+router.get('/static/epub/:id', requireLogin, (req, res) => {
+  try {
+    res.sendFile(
+      path.join(__dirname, '..', '..', 'uploads', 'epub', req.params.id),
+    );
+  } catch (e) {
+    res.status(400).json({ error: getErrorMessage(e) });
+  }
+});
