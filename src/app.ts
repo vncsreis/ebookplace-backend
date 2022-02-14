56a79dc176698bfe7afeea464342fd24f25ebe27
Vinícius <euphonion@tutanota.com>
2022-02-13 23:32:03 -0300

    Added CORS

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import express from 'express';
 import { router } from './routes';
+import cors, { CorsOptions } from 'cors';
+import dotenv from 'dotenv-safe';
 
 export const app = express();
+
+const env = dotenv.config();
+
+const corsOptions: CorsOptions = {
+  origin: env.required.CORSORIGIN,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
